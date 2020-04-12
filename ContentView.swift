f2c33cbd108aacc5785af05a398f791e13d56686
Nick O'Leary <noleary82@gmail.com>
2020-04-11 20:32:26 -0400

    should work now

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Honk-API-Frontend
+//
+//  Created by Nicholas O'Leary on 4/11/20.
+//  Copyright © 2020 Honk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
