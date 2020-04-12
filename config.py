b45cba027b5609fd937f6db8af6c14fb18a5e267
ben vandenbosch <benjaminrvandenbosch@gmail.com>
2020-04-12 14:08:20 -0700

    try to get database url correct

--- config.py
+++ config.py
@@ -12,8 +12,10 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'ha will not guess this'
 
     # Set the location of the db (used by Flask-SQLAlchemy extension)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+    print(SQLALCHEMY_DATABASE_URI)
+    print(basedir)
 
     # Disable signal to application each time change is made to db
     SQLALCHEMY_TRACK_MODIFICATIONS = False
