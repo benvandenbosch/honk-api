23302a671246161ad0a95ce79c1175a5822eba9d
ben vandenbosch <benjaminrvandenbosch@gmail.com>
2020-04-29 14:21:26 -0700

    only make apns client if in prod

--- app/__init__.py
+++ app/__init__.py
@@ -11,12 +11,14 @@ db = SQLAlchemy()
 migrate = Migrate()
 
 # Create a global APNsClient object
-apns_client = APNsClient(
-    team_id=os.environ.get('APNS_TEAM_ID') or 'test-fake',
-    bundle_id=os.environ.get('APNS_BUNDLE_ID') or 'test-fake',
-    auth_key_id=os.environ.get('APNS_AUTH_KEY_ID') or 'test-fake',
-    auth_key=os.environ.get('APNS_KEY') or 'test-fake'
-)
+apns_client = ''
+if os.environ.get('ENV_NAME') == 'PROD':
+    apns_client = APNsClient(
+        team_id=os.environ.get('APNS_TEAM_ID') or 'test-fake',
+        bundle_id=os.environ.get('APNS_BUNDLE_ID') or 'test-fake',
+        auth_key_id=os.environ.get('APNS_AUTH_KEY_ID') or 'test-fake',
+        auth_key=os.environ.get('APNS_KEY') or 'test-fake'
+    )
 
 # Use the Config class from config.py module to create an instance of the app
 def create_app(config_class=Config):
